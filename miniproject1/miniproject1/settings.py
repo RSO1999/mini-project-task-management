01a528b75a8eba2a865bf2b77def9065ba57da44
AlxZba <alex@DIABLO>
2024-09-11 23:32:14 -0500

    Created the DB Dockerfile, check the readme

--- miniproject1/miniproject1/settings.py
+++ miniproject1/miniproject1/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'miniproject1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST' : 'localhost',
-        'PORT' : '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
