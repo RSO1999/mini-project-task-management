5c0b0d9cede649402da6a0cc1cf917d2b48ad2be
AlxZba <azieba4@depaul.edu>
2024-09-13 18:34:43 -0500

    migrated DB to use agreed-upon model for todo Items

--- miniproject1/todolist5/models.py
+++ miniproject1/todolist5/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class TodoItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=50)
+    LEVEL_CHOICES = [
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) #FOREIGN KEY
+    
+    title = models.CharField(max_length=50, null=False, blank=False, default="My Task")
     description = models.TextField(max_length=500, null=True, blank=True)
     completed = models.BooleanField(default=False)
-    due_date = models.DateTimeField()
-    priority = models.CharField(max_length=10)
+    due_date = models.DateField(default=datetime.date.today)
+    priority = models.CharField(max_length=10, choices=LEVEL_CHOICES, default='M')
     
     def __str__(self):
         return self.title
