0351593bcd5299d3aa72af6b62a8fc336fe94b83
AlxZba <alex@DIABLO>
2024-09-13 18:20:10 -0500

    created Database Schema

--- miniproject1/todolist5/models.py
+++ miniproject1/todolist5/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 class TodoItem(models.Model):
-    title = models.CharField(max_length=200)
-    completed=models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateTimeField()
+    priority = models.CharField(max_length=10)
     
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering=['due_date']
