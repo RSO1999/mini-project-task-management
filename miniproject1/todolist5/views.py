b88ef25ca6fb51ab446b60b13b22fd601718eb38
Jamya Whitman <jamyawhitman0802@gmail.com>
2024-09-16 01:36:01 -0500

    Create Todo, Delete Todo, Set priority

--- miniproject1/todolist5/views.py
+++ miniproject1/todolist5/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
-from .models import TodoItem  # Import the TodoItem model
+from .models import TodoItem 
 from django.http import HttpResponseRedirect
 from .forms import TodoItemForm
 from django.urls import reverse_lazy
@@ -13,32 +13,24 @@ def home(request):
     todos = TodoItem.objects.all()
     return render(request, "home.html", {'todos': todos})
 
-    #if request.user.is_authenticated:
-     #   todos = TodoItem.objects.filter(user=request.user).order_by('due_date')
-    #else:
-    #    todos = []
     
-    #return render(request, 'home.html', {'todos': todos})
-
 class AddTodoItemView(FormView):
-    template_name = 'add_todo_item.html'  # The template for displaying the form
-    form_class = TodoItemForm # The form to use
-    success_url = reverse_lazy('home')  # URL to redirect to after successful form submission
+    template_name = 'add_todo_item.html'
+    form_class = TodoItemForm 
+    success_url = reverse_lazy('home')
 
-    # Override form_valid method to save the form and assign the user
     def form_valid(self, form):
-        todo_item = form.save(commit=False)  # Do not commit yet
+        todo_item = form.save(commit=False)  
         if self.request.user.is_authenticated:
-            todo_item.user = self.request.user  # Assign the logged-in user
-        todo_item.save()  # Now save the object
-        return super().form_valid(form)  # Continue with the default behavior
+            todo_item.user = self.request.user  
+        todo_item.save()  
+        return super().form_valid(form)
 
 def delete_todo_item(request):
     todo_items = TodoItem.objects.all()
     return render(request, 'deleteTodo.html', {'todo_items': todo_items})
 
 class BulkDeleteTodoView(View):
-    #template_name = 'confirmDelete.html'
     success_url = reverse_lazy('delete_todo_item')
 
     def post(self,request, *args, **kwargs):
